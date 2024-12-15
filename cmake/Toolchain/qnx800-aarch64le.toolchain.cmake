8f5ec8d9dc4bf3eaa4be64b9f8dd260f3fe55a3a
Deniz Eren <deniz.eren@outlook.com>
2024-12-15 13:13:18 +1100

    Added CMake toolchain qnx800-aarch64le.toolchain.cmake

--- cmake/Toolchain/qnx800-aarch64le.toolchain.cmake
+++ cmake/Toolchain/qnx800-aarch64le.toolchain.cmake
@@ -1,5 +1,5 @@
-# \file     qnx800-x86_64.toolchain.cmake
-# \brief    CMake QNX 8.0 cross-compile toolchain for x86_64 platform.
+# \file     qnx800-aarch64le.toolchain.cmake
+# \brief    CMake QNX 8.0 cross-compile toolchain for aarch64le platform.
 #
 # Copyright (C) 2025 Deniz Eren (deniz.eren@outlook.com)
 #
@@ -19,30 +19,30 @@
 
 # the name of the target operating system
 set( CMAKE_SYSTEM_NAME qnx800 )
-set( arch x86_64 )
+set( arch aarch64le )
 
 # Set toolchain prefix to find the correct tools
-set( _CMAKE_TOOLCHAIN_PREFIX    $ENV{QNX_HOST}/usr/bin/ntox86_64- )
+set( _CMAKE_TOOLCHAIN_PREFIX    $ENV{QNX_HOST}/usr/bin/ntoaarch64le- )
 
 # which compilers to use for C and C++
 set( CMAKE_C_COMPILER           $ENV{QNX_HOST}/usr/bin/qcc )
 set( CMAKE_C_COMPILER_TARGET    gcc_nto${arch} )
-set( CMAKE_CXX_COMPILER         $ENV{QNX_HOST}/usr/bin/ntox86_64-c++ )
+set( CMAKE_CXX_COMPILER         $ENV{QNX_HOST}/usr/bin/ntoaarch64-c++ )
 # TODO: investigate why setting q++ doesn't work:
 #set( CMAKE_CXX_COMPILER         $ENV{QNX_HOST}/usr/bin/q++ )
 set( CMAKE_CXX_COMPILER_TARGET  gcc_nto${arch} )
 
 # Set some known tools
-set( CMAKE_NM       $ENV{QNX_HOST}/usr/bin/ntox86_64-nm )
-set( CMAKE_AR       $ENV{QNX_HOST}/usr/bin/ntox86_64-ar )
-set( CMAKE_READELF  $ENV{QNX_HOST}/usr/bin/ntox86_64-readelf )
-set( CMAKE_STRIP    $ENV{QNX_HOST}/usr/bin/ntox86_64-strip )
+set( CMAKE_NM       $ENV{QNX_HOST}/usr/bin/ntoaarch64le-nm )
+set( CMAKE_AR       $ENV{QNX_HOST}/usr/bin/ntoaarch64le-ar )
+set( CMAKE_READELF  $ENV{QNX_HOST}/usr/bin/ntoaarch64le-readelf )
+set( CMAKE_STRIP    $ENV{QNX_HOST}/usr/bin/ntoaarch64le-strip )
 
 # where is the target environment located
 set( CMAKE_FIND_ROOT_PATH
     $ENV{QNX_TARGET}/usr
-    $ENV{QNX_TARGET}/x86_64
-    $ENV{QNX_TARGET}/x86_64/usr )
+    $ENV{QNX_TARGET}/aarch64le
+    $ENV{QNX_TARGET}/aarch64le/usr )
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
@@ -53,11 +53,11 @@ set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
 set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
 
 # set gcov executable
-set( QNX_GCOV_EXE $ENV{QNX_HOST}/usr/bin/ntox86_64-gcov )
+set( QNX_GCOV_EXE $ENV{QNX_HOST}/usr/bin/ntoaarch64le-gcov )
 
 # set profiling library
-set( QNX_PROFILING_LIBRARY $ENV{QNX_TARGET}/x86_64/usr/lib/libprofilingS.a )
+set( QNX_PROFILING_LIBRARY $ENV{QNX_TARGET}/aarch64le/usr/lib/libprofilingS.a )
 
 # Googletest
-set( ENV{GTEST_ROOT} $ENV{QNX_TARGET}/x86_64/usr/ )
-set( GTEST_MAIN_LIBRARY $ENV{QNX_TARGET}/x86_64/usr/lib/libgtest_main.so )
+set( ENV{GTEST_ROOT} $ENV{QNX_TARGET}/aarch64le/usr/ )
+set( GTEST_MAIN_LIBRARY $ENV{QNX_TARGET}/aarch64le/usr/lib/libgtestS.a )
